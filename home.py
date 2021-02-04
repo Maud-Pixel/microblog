911db10af59c47c903359db27b5f42deee71aee4
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-04 15:24:38 +0100

    first commit

--- home.py
+++ home.py
@@ -3,37 +3,38 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://maud:test@cluster0.p4vc7.mongodb.net/microblog?retryWrites=true&w=majority",ssl=True, ssl_cert_reqs="CERT_NONE") 
+    app.db= client.microblog
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://maud:test@cluster0.p4vc7.mongodb.net/microblog?retryWrites=true&w=majority",ssl=True, ssl_cert_reqs="CERT_NONE") 
-app.db= client.microblog
 
+    @app.route("/", methods=["GET","POST"])
+    def home():
+      if request.method == "POST":
+        entry_content = request.form.get("message")
+        formatted_dates = datetime.date.today().strftime("%Y-%m-%d")
+        app.db.entries.insert({"content": entry_content, "date": formatted_dates})
 
-@app.route("/", methods=["GET","POST"])
-def home():
-  if request.method == "POST":
-    entry_content = request.form.get("message")
-    formatted_dates = datetime.date.today().strftime("%Y-%m-%d")
-    app.db.entries.insert({"content": entry_content, "date": formatted_dates})
+      entries_with_date = [
+        (
+          entry['content'],
+          entry['date'],
+          datetime.datetime.strptime(entry['date'],"%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+      ]
+      return render_template("home.html", entries=entries_with_date)
 
-  entries_with_date = [
-    (
-      entry['content'],
-      entry['date'],
-      datetime.datetime.strptime(entry['date'],"%Y-%m-%d").strftime("%b %d")
-    )
-    for entry in app.db.entries.find({})
-  ]
-  return render_template("home.html", entries=entries_with_date)
-
-@app.route("/hello")
-def first_page():
-  name= "Maud"
-  template_name="test"
-  kwargs = {
-      "name":name,
-      "template_name":template_name
-  }
-  return render_template("test.html", **kwargs)
+    @app.route("/hello")
+    def first_page():
+      name= "Maud"
+      template_name="test"
+      kwargs = {
+          "name":name,
+          "template_name":template_name
+      }
+      return render_template("test.html", **kwargs)
   
+    return app
   
