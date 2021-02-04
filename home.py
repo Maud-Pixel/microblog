b369917ac5a38d6254303ae10e13fa5bffab6e0a
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-04 17:19:00 +0100

    test

--- home.py
+++ home.py
@@ -2,19 +2,18 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-
-home = Flask(__name__)
+app = Flask(__name__)
 client = MongoClient("mongodb+srv://maud:test@cluster0.p4vc7.mongodb.net/microblog?retryWrites=true&w=majority",
                      ssl=True, ssl_cert_reqs="CERT_NONE")
-home.db = client.microblog
+app.db = client.microblog
 
 
-@home.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("message")
         formatted_dates = datetime.date.today().strftime("%Y-%m-%d")
-        home.db.entries.insert(
+        app.db.entries.insert(
             {"content": entry_content, "date": formatted_dates})
 
     entries_with_date = [
@@ -24,12 +23,12 @@ def home():
             datetime.datetime.strptime(
                 entry['date'], "%Y-%m-%d").strftime("%b %d")
         )
-        for entry in home.db.entries.find({})
+        for entry in app.db.entries.find({})
     ]
     return render_template("home.html", entries=entries_with_date)
 
 
-@home.route("/hello")
+@app.route("/hello")
 def first_page():
     name = "Maud"
     template_name = "test"
