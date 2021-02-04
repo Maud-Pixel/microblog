9988f3f99836d4526f6439a2b8aea6d13ceeabe4
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-04 15:06:55 +0100

    first commit

---
+++ home.py
@@ -0,0 +1,39 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://maud:test@cluster0.p4vc7.mongodb.net/microblog?retryWrites=true&w=majority",ssl=True, ssl_cert_reqs="CERT_NONE") 
+app.db= client.microblog
+
+
+@app.route("/", methods=["GET","POST"])
+def home():
+  if request.method == "POST":
+    entry_content = request.form.get("message")
+    formatted_dates = datetime.date.today().strftime("%Y-%m-%d")
+    app.db.entries.insert({"content": entry_content, "date": formatted_dates})
+
+  entries_with_date = [
+    (
+      entry['content'],
+      entry['date'],
+      datetime.datetime.strptime(entry['date'],"%Y-%m-%d").strftime("%b %d")
+    )
+    for entry in app.db.entries.find({})
+  ]
+  return render_template("home.html", entries=entries_with_date)
+
+@app.route("/hello")
+def first_page():
+  name= "Maud"
+  template_name="test"
+  kwargs = {
+      "name":name,
+      "template_name":template_name
+  }
+  return render_template("test.html", **kwargs)
+  
+  
